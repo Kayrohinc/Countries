84078462d5d647f29f7ee54fd26f8d6439138309
Kayrohinc <kayrohinc@gmail.com>
2014-07-02 15:49:47 +0400

    2

---
+++ Assets/Scripts/CountryParser.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using Newtonsoft.Json;
+using UnityEditor;
+using System.Collections.Generic;
+namespace Country
+{
+    public class Parametrs
+    {
+
+        public string IconPath;
+        public string Name;
+        public string OfficialName;
+        public string Code31;
+
+
+
+    
+ }
+
+    public class CountryParser:MonoBehaviour
+    {
+        public TextAsset JsonText;
+        public static System.Collections.Generic.List<Parametrs> ListOfCountries;
+        public void PushAllToList()
+        {
+            ListOfCountries = new List<Parametrs>();
+            List<Dictionary<string, string>> temp = JsonConvert.DeserializeObject <List<Dictionary<string, string>>>(JsonText.text);
+            foreach (Dictionary<string,string> foreachtemp in  temp)
+            {
+                Parametrs tempPar = new Parametrs();
+               tempPar.Code31 = foreachtemp["code3l"];
+                tempPar.Name = foreachtemp["name"];
+                tempPar.OfficialName = foreachtemp["official_name"];
+                tempPar.IconPath = foreachtemp["code2l"].ToLower();
+                ListOfCountries.Add(tempPar);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UnityCountryParser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using Country;
+public class UnityCountryParser :CountryParser {
+    void Awake()
+    {
+       
+        base.PushAllToList();
+       Debug.Log(ListOfCountries[0].Name);
+       Debug.Log(ListOfCountries[0].IconPath);
+      Texture2D temp=Resources.Load(ListOfCountries[0].IconPath) as Texture2D;
+      GetComponent<GUITexture>().texture = temp;
+    }
+   
+}
